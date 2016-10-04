32b87c1fdf85b26d380ef2bf141842bfc5877f79
Jack Lightfoot <jack.lightfoot@tee2.org>
2016-10-04 11:11:37 -0700

    added default page for unmatched routes - sort of like a 404 page

--- src/app/PlainRoutes.js
+++ src/app/PlainRoutes.js
@@ -78,7 +78,8 @@ const rootRoute = [
     name: 'root',
     childRoutes: [
       require('./routes/quickLoadRoute.js').default,
-      require('./routes/mainPageRoute.js').default
+      require('./routes/mainPageRoute.js').default,
+      require('./routes/notFoundRoute.js').default
     ]
   }
 ];
